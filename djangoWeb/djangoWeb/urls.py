5394a1234fb476a8686f67abf0988c7574b49b82
Jean-Daniel Liwanga <jeandaniel.liwanga@gmail.com>
2024-04-02 18:28:40 -0400

    Updated urls.py
    Added a space

--- djangoWeb/djangoWeb/urls.py
+++ djangoWeb/djangoWeb/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import include,path
 
 urlpatterns = [
+    
     path('usmapp/', include('usmapp.urls')),
     path('admin/', admin.site.urls),
 ]
